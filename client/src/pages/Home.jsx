4f172c75ddfb491527b929a523ef55a3edfaac0d
Rishi <rishimaheshwari040@gmail.com>
2024-08-25 13:38:28 +0530

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Company from "../components/core/Home/Company";
 import Services from "../components/core/Home/Services";
 import { Fade } from "react-awesome-reveal";
 import CommanText from "../components/core/Home/CommanText";
+import TagCloudWithDetails from "../components/core/Home/CanvasRotation";
+import Gallery from "../components/core/Home/Gallery";
 
 function Home() {
   return (
