5a2d20f67acde9c417615a9b8eeb2618893a0510
Rishi <rishimaheshwari040@gmail.com>
2024-08-27 12:50:08 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,11 @@ import Modal from "./components/core/Home/Modal";
 import { Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-import ConstructorHome from "./pages/constructor/ConstructorHome";
-import ConstructorAbout from "./pages/constructor/ConstructorAbout";
-import ConstructorContact from "./pages/constructor/ConstructorContact";
+
 import Service from "./pages/Service";
 import RISIFoddHome from "./Routes/RISIFoddHome";
 import MilkProduct from "./Routes/MilkProduct";
+import Constroctor from "./Routes/Constroctor";
 function App() {
   const [showModal, setShowModal] = useState(true);
 
@@ -31,10 +30,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Service />} />
-        <Route path="/construction-home" element={<ConstructorHome />} />
-        <Route path="/construction-about" element={<ConstructorAbout />} />
-        <Route path="/construction-contact" element={<ConstructorContact />} />
-
+        <Route path="/*" element={<Constroctor />} />
 
         <Route path="/ri-si-food/*" element={<RISIFoddHome />} />
         <Route path="/milk/*" element={<MilkProduct />} />
---
+++ client/src/Routes/Constroctor.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ConstructorHome from "../pages/constructor/ConstructorHome";
+import ConstructorAbout from "../pages/constructor/ConstructorAbout";
+import ConstructorContact from "../pages/constructor/ConstructorContact";
+const Constroctor = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/construction-home" element={<ConstructorHome />} />
+        <Route path="/construction-about" element={<ConstructorAbout />} />
+        <Route path="/construction-contact" element={<ConstructorContact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Constroctor;
