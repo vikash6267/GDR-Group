cbb78288c67947457d2bd35352a89b3b43427f85
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-09-04 13:02:08 +0530

    fix video

--- client/src/components/core/Home/CommanText.jsx
+++ client/src/components/core/Home/CommanText.jsx
@@ -5,36 +5,21 @@ import video from "../../../assests/homevideo.mp4"
 const ScrollingText = () => {
   const [isTypingDone, setIsTypingDone] = useState(false);
 
-  useEffect(() => {
-    const handleAnimationEnd = () => {
-      setIsTypingDone(true);
-    };
-
-    document
-      .querySelector(".type-animation")
-      .addEventListener("animationend", handleAnimationEnd);
-
-    return () => {
-      document.querySelector(".type-animation");
-      // .removeEventListener("animationend", handleAnimationEnd);
-    };
-  }, []);
 
   return (
-    <div className="relative h-32 w-full overflow-hidden bg-black">
+    <div className="relative  w-full overflow-hidden bg-black">
       {/* Background Image */}
-      <div className="absolute top-0 left-0 w-full h-full">
+      {/* <div className="absolute top-0 left-0 w-full h-full">
         <img
           className="w-full h-full object-cover"
           src={images}
           alt="background"
         />
-        {/* Darker Overlay */}
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60"></div>
-      </div>
+      </div> */}
 
       {/* Scrolling Texts */}
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center">
+      <div className="">
         {/* <div className="overflow-hidden w-full h-16">
           <div className="scrolling-text-right hover:pause-scroll">
             If you want to join us and invest, then you are welcome to GDR Group
@@ -46,7 +31,7 @@ const ScrollingText = () => {
           </div>
         </div> */}
 
-        <div className="w-11/12 mx-auto uppercase">
+        {/* <div className="w-11/12 mx-auto uppercase">
           <TypeAnimation
             sequence={[
               "Welcome To GDR Group",
@@ -66,7 +51,9 @@ const ScrollingText = () => {
             className="type-animation lg:text-3xl text-xl font-bold text-white"
             onTypingDone={() => setIsTypingDone(true)} // Set typing done when text animation completes
           />
-        </div>
+        </div> */}
+
+        <video src={video} autoPlay muted></video>
       </div>
     </div>
   );
